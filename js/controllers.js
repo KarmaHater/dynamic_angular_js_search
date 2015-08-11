ccc7c75544c853451273fe9746b143e7617bb451
Adnard-kod <andra.lally@gmail.com>
2015-08-11 13:53:13 +0100

    adding files stucture

---
+++ js/controllers.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('data.json').success(function(data){
+  $scope.artists = data;
+
+  })
+}]);
