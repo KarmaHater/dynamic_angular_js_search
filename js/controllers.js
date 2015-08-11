54d02c1ef4cb3aa78dc93e4cb46b9b05275223f0
Adnard-kod <andra.lally@gmail.com>
2015-08-11 20:30:08 +0100

    sorting ability added

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyCtrl', ['$scope', '$http', function ($scope, $http) {
   $http.get('data.json').success(function(data){
   $scope.artists = data;
+  $scope.artistOrder = 'name';
 
   })
 }]);
